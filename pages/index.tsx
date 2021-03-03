ba82c6552dddfddb4abc9b16e18d9fcc71662c25
ElodieIdaLaps <59149456+ElodieLaps@users.noreply.github.com>
2021-03-03 11:01:31 +0100

    base style implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Layout from '../components/page/Layout'
+import styles from '../styles/Home.module.scss'
+
 
 export default function Home() {
   return (
     <>
       <Layout pageTitle='accueil'>
-        <h1>Accueil</h1>
+        <div className={styles.container}>
+
+        </div>
       </Layout>
     </>
   )
